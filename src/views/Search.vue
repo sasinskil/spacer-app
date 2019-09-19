3c3f78c52415033f4f87dd490f87b9142a09cf5f
sasinskil <sasinskil@gmail.com>
2019-09-19 19:41:37 +0200

    create Search component, basic view

---
+++ src/views/Search.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="container">
+    <div class="search">
+      <label class="search__label" for="search">Search</label>
+      <input 
+        class="search__input" 
+        type="text" 
+        id="search" 
+        name="search" 
+        v-model="searchValue"
+        @input="handleInput"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "Search",
+  data() {
+    return {
+      searchValue: ""
+    };
+  },
+  methods: {
+    handleInput() {
+      console.log(this.searchValue);
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+
+.container {
+  width: 100%;
+  margin: 0;
+  padding: 2rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.search {
+  display: flex;
+  flex-direction: column;
+  width: 250px;
+
+  &__label {
+    text-align: left;
+  }
+
+  &__input {
+    padding: 0.5rem;
+    border: none;
+    border-bottom: 1px solid black;
+  }
+}
+</style>
